fee80660da1bacc7da92b66bde79b491527afd9f
Stony Gomes Guimarães <69357791+stonyguimaraes@users.noreply.github.com>
2024-05-06 10:39:21 -0300

    Add files via upload

---
+++ DesafioControleFluxo/src/DesafioControleFluxo/ParametrosInvalidosException.java
@@ -0,0 +1,9 @@
+package DesafioControleFluxo;
+
+
+	class ParametrosInvalidosException extends Exception {
+	    public ParametrosInvalidosException(String message) {
+	        super(message);
+	    }
+	}
+
